77ee448a1525f405a88375ff0023c148273f1102
binhlam-eh <binhlam.thai@employmenthero.com>
2024-05-29 21:44:56 +0700

    update

--- app/tests/utils.py
+++ app/tests/utils.py
@@ -10,7 +10,7 @@ from ..services.security.utils import bcrypt_context
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
